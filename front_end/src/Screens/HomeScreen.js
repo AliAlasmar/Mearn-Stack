645ca29836229f638c164265d3653d4d50edac69
Ali_Alasmar <alialasmar522@gmail.com>
2023-04-11 17:11:50 +0300

    api from backend

--- front_end/src/Screens/HomeScreen.js
+++ front_end/src/Screens/HomeScreen.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import data from '../Data/data'
+import { useState , useEffect} from 'react'
+import axios from 'axios'
 function HomeScreen() {
+    let [product , setProduct] = useState({
+        products : []
+    })
+
+    async function getproduct() {
+      
+        //  const response = fetch('http://localhost:3001/api1/product')
+          fetch('http://localhost:3001/api1/product')
+      .then(response => response.json())
+      .then(json =>{
+         console.log(json)
+         setProduct({
+            products : json
+         })
+      }
+         )
+       
+          .catch(err => console.log(err))
+        
+      }
+
+    useEffect(()=>{
+        getproduct()
+    },[])
     return (
         <div>
             List Product
             <h1>Feature Product</h1>
             <div className="products">
-                {data.products.map((product) => (
+                {product.products.map((product) => (
                     <div className="product" key={product.slug}>
                         <Link  to={`/product/${product.slug}`}>
                             <img src={product.image} alt={product.name} />
